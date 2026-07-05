ca0460fe53ec8c3679252f492956ba3b66f4fd88
yehoshua <y0587828030@gmail.com>
2026-07-05 11:20:19 +0300

    step 6-7

--- lambda.py.py
+++ lambda.py.py
@@ -8,16 +8,26 @@
 
 
 # ##step 3
-# full_name = lambda first_name, last_name:  first_name + last_name
-# print("yehosh","zaltz")
+# full_name = lambda first_name, last_name:  f"{first_name} {last_name}"
+# print(full_name("yehosh","zaltz"))
 
 ##step 4
 # grade_status = lambda grade: f"pass" if grade >= 55 else "fail"
 # print(grade_status(50))
 # print(grade_status(90))
 
-#step 5
-larger = lambda num1 , num2 : num1 if num1 > num2 else num2
-print(larger(10,5))
-print(larger(10,100))
-print(larger(10,10))
+# #step 5
+# larger = lambda num1 , num2 : num1 if num1 > num2 else num2
+# print(larger(10,5))
+# print(larger(10,100))
+# print(larger(10,10))
+# print(larger(100,10))
+
+# ##step 6
+# distance_from_10 = lambda num1 : 10 - num1 if num1 < 10 else num1 - 10
+# print(distance_from_10(100))
+
+# ## step 7
+item_total = lambda item: item["price"] * item["amount"]
+
+print(item_total({"name": "Pen", "price": 5, "amount": 10}))
